f3fd9932d5e00763f90406a4b6e468437e6e7c71
realsale <pwedesaken@gmail.com>
2022-07-22 23:30:00 +0800

    chore: add custom utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      desktop: "1440px"
+    },
+    extend: {
+      colors: {
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        "cream": "hsl(30, 38%, 92%)",
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)"
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        fraunces: ["Fraunces", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
