629aa4fb107cca6036307b8e7e8aa11d5a4a925a
realsale <pwedesaken@gmail.com>
2022-07-23 20:05:12 +0800

    feat: add desktop design

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       },
       colors: {
         "dark-cyan": "hsl(158, 36%, 37%)",
+        "very-dark-cyan": "hsl(156, 42%, 18%)",
         "cream": "hsl(30, 38%, 92%)",
         "very-dark-blue": "hsl(212, 21%, 14%)",
         "dark-grayish-blue": "hsl(228, 12%, 48%)"
