ff6c6945c0f41e8d7da39b39da7426df9161b093
realsale <pwedesaken@gmail.com>
2022-07-28 21:31:13 +0800

    feat: add mobile design

--- challenges/nft-preview-card-component/tailwind.config.js
+++ challenges/nft-preview-card-component/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "hsl(215, 51%, 70%)",
+        "cyan": "hsl(178, 100%, 50%)",
+        "dark-blue": {
+          DEFAULT: "hsl(217, 54%, 11%)",
+          card: "hsl(216, 50%, 16%)",
+          line: "hsl(215, 32%, 27%)"
+        }
+      },
+      fontFamily: {
+        outfit: ["Outfit", "serif"]
+      }
+    },
   },
   plugins: [],
 }
